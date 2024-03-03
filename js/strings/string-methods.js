33c674632c02d874ddaf29c360cd0decc17c884a
bliss <107047190+Blisse1@users.noreply.github.com>
2024-03-02 20:46:16 -0500

    string-methods I

---
+++ js/strings/string-methods.js
@@ -0,0 +1,71 @@
+const string = "This is my string";
+// Your variable becomes a string object instance.
+
+// Length of an array; .length
+const browserType = "mozilla";
+browserType.length;
+
+// Retrieve the last character of any string
+browserType[browserType.length - 1];
+
+// Test if a string contains a substring; .includes()
+
+if(browserType.includes("zilla")){
+    console.log("Found zilla!");
+}else {
+    console.log("No zilla here");
+}
+
+// If a string starts or ends with a particular substring; startsWith(), endsWith()
+
+if(browserType.startsWith("zilla")){
+    console.log("Found zilla!");
+}else {
+    console.log("No zilla here");
+}
+
+if(browserType.endsWith("zilla")){
+    console.log("Found zilla!");
+}else {
+    console.log("No zilla here");
+}
+
+// Finding the position of a substring in a string; indexOf()
+// returns -1 if the substring is not present in the string
+
+const tagLine = "MDN - Resources for developers by developers";
+console.log(tagLine.indexOf("x"));
+
+// How to find subsequent ocurrences
+
+const firstOccurrence = tagLine.indexOf("developers");
+const secondOccurrence = tagLine.indexOf("developers", firstOccurrence + 1);
+console.log(firstOccurrence);
+console.log(secondOccurrence);
+
+// Extracting a substring from a string; slice()
+
+const browserType2 = "mozilla";
+console.log(browserType2.slice(1, 4));
+
+// extract all the remaining characters in a string after a certain character
+// remember that slice doesnt count the last character but the first one
+
+console.log(browserType2.slice(2)) // zilla
+
+// Changing the case; toLowerCase(), toUpperCase()
+
+const radData = "My NaMe is mUD";
+console.log(radData.toLowerCase());
+console.log(radData.toUpperCase());
+
+// Updating parts of a string; replace()
+// You can replace one substring inside a string with another substring
+
+const myBrowser = "chrome";
+const updated = myBrowser.replace("chrome", "firefox");
+
+console.log(updated);
+console.log(myBrowser);
+
+
