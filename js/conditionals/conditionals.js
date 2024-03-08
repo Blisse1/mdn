bc8fe94e735e0320d37244d13a2720f4bc5f120a
bliss <107047190+Blisse1@users.noreply.github.com>
2024-03-08 13:36:29 -0500

    JS project

---
+++ js/conditionals/conditionals.js
@@ -0,0 +1,16 @@
+// This code statement always runs
+// regardless of whether the condition returns true or false
+
+if(condition){
+    
+}
+
+// You may sometimes see if...else statements written without curly braces
+
+if(condition) // code to run if condition is true
+else {} // run some other code instead
+
+// It's much easier to understand the code if you use curly braces to delimit
+// the blocks of code
+//
+// Por lo general se elije if else para condiciones complejas :
