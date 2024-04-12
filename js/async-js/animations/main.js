6caee8bc8a9676ad26380d8e8b26f350b1e78314
bliss <107047190+Blisse1@users.noreply.github.com>
2024-04-11 19:53:53 -0500

    fix animation task thanks to wbamberg

--- js/async-js/animations/main.js
+++ js/async-js/animations/main.js
@@ -24,45 +24,46 @@ const alice3 = document.querySelector("#alice3");
 //     })
 
 // callback version
-// function animate1(callback){
-//     const a1 = alice1.animate(aliceTumbling, aliceTiming).finished;
-//     callback(a1)
-// }
-
-// function animate2(callback){
-//     const a2 = alice2.animate(aliceTumbling, aliceTiming).finished;
-//     callback(a2)
-// }
-
-// function animate3(callback){
-//     const a3 = alice3.animate(aliceTumbling, aliceTiming);
-//     callback(a3);
-// }
-
-// function doAnimation(){
-//     animate1(() => {
-//         setTimeout(() => {
-//             animate2(() => {
-//                 setTimeout(() => {
-//                     animate3(() => {
-//                     });
-//                 }, 2000)
-//             });    
-//         }, 2000)
-//     })
-// }
-
-// doAnimation();
-
-// async await version
-async function animate() {
-    try{
-         await alice1.animate(aliceTumbling, aliceTiming).finished;
-         await alice2.animate(aliceTumbling, aliceTiming).finished;
-         alice3.animate(aliceTumbling, aliceTiming);
-    }catch(err){
-       console.log(err);
-    }
+function animate1(callback){
+    const a1 = alice1.animate(aliceTumbling, aliceTiming).finished;
+    callback(a1)
 }
 
-animate();
+function animate2(callback){
+    const a2 = alice2.animate(aliceTumbling, aliceTiming).finished;
+    callback(a2)
+}
+
+function animate3(){
+    const a3 = alice3.animate(aliceTumbling, aliceTiming).finished;
+    return a3;
+}
+
+function doAnimation(){
+    animate1((callback) => {
+        callback
+            .then(() => {
+                animate2((callback) => {
+                    callback
+                        .then(() => {
+                            animate3();
+                        })
+                })
+            })
+    })
+}
+
+doAnimation();
+
+// async await version
+// async function animate() {
+//     try{
+//          await alice1.animate(aliceTumbling, aliceTiming).finished;
+//          await alice2.animate(aliceTumbling, aliceTiming).finished;
+//          alice3.animate(aliceTumbling, aliceTiming);
+//     }catch(err){
+//        console.log(err);
+//     }
+// }
+
+// animate();
---
+++ js/client-side-apis/intro-apis.js
@@ -0,0 +1,11 @@
+// API
+// APIs are constructors made available in programming languages to
+// allow developers to create complex functionality more easily.
+// They abstract more complex code away from you, providing some easier syntax
+// to use in its place.
+
+// Difference between a library and a framework
+// The key difference between a library and a framework is "Inversion of Control"
+// When calling a method from a library, the developer is in control.
+// With a framework, the control is inverted. The framework calls the developers code.
+//
